8cfa4d32505a8b2174b1fad3a5604075a0b2d628
PEA AN <peaanedu@gmail.com>
2026-05-06 16:26:45 +0700

    Deploy api v7

--- api/microservice-project/products-service/app.py
+++ api/microservice-project/products-service/app.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 DB_HOST = os.getenv("DB_HOST", "postgres")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "products_db")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 
 
 def get_connection():
